ec12008637dad6be198d7a388601719c2965a85f
Viraj Bhosale <viraj.bhosale@macys.com>
2018-05-17 17:57:04 +0530

    Move character withing the board limits and place 3 bugs at random speed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+	let speeds = [2,3,5];
+	this.step = speeds[Math.floor(Math.random()*3)];
+	this.x = 0;
+	this.y = row*83;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,7 +17,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	this.x+=this.step;
 };
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -24,12 +29,54 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+	this.default_x_move = 101;
+	this.default_y_move = 83;
+	this.x = 0;
+	this.y = 0;
+	this.sprite = "images/char-boy.png";
+};
 
+Player.prototype.update = function(){
+	console.log("Player at x="+this.x+", y="+this.y);
+};
 
+Player.prototype.render = function(){
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPressed){
+	switch(keyPressed){
+		case 'left': 
+			this.updateX(this.x-this.default_x_move);
+			break;
+		case 'right':
+			this.updateX(this.x+this.default_x_move);
+			break;
+		case 'up':
+			this.updateY(this.y-this.default_y_move);
+			break;
+		case 'down':
+			this.updateY(this.y+this.default_y_move);
+		
+	}
+};
+
+Player.prototype.updateX = function(newX){
+	if(newX >=0 && newX <= 404) this.x = newX;
+}
+
+Player.prototype.updateY = function(newY){
+	if(newY >=0 && newY <= 415) this.y = newY;
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [  new Enemy(1),
+					new Enemy(2),
+					new Enemy(3)
+					];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
